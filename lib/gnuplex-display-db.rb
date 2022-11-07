ff7464e694b019d69547b3af371c9c195a4e6ba0
j <j>
2022-11-06 19:56:10 -0600

    Reorganize GNUPlexDisplayserver

--- lib/gnuplex-display-db.rb
+++
@@ -1,8 +0,0 @@
-class DB
-  # def self.f
-  #    puts "FUCKYEAH"
-  # end
-  def db
-    @db ||= SQLite3::Database.new "gnuplex.sqlite3"
-  end
-end
--- lib/gnuplex-display-server.rb
+++ lib/gnuplex-display-server.rb
@@ -1,6 +1,6 @@
 class GNUPlexDisplayServer
   def self.run
-    pid = spawn("mpv --idle=yes --input-ipc-server=/tmp/mpvsocket --fs")
-    Process.wait pid
+    mpv_proc = spawn("mpv --idle=yes --input-ipc-server=/tmp/mpvsocket --fs")
+    Process.wait mpv_proc
   end
 end
