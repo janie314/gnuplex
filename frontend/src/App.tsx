caa48a4eab038982dd2c544eb3cb713b7a9ac86c
j <j>
2022-11-12 00:30:18 -0600

    A frontend that can play and pause

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,10 +4,25 @@ import { mediafiles } from "./mediafiles";
 import serverconfig from "../serverconfig.json";
 
 function App() {
-  
+  function play() {
+    return fetch(
+      `http://${serverconfig.hostname}/api/play`,
+      { method: "POST" },
+    );
+  }
+
+  function pause() {
+    return fetch(
+      `http://${serverconfig.hostname}/api/pause`,
+      { method: "POST" },
+    );
+  }
+
   function queue(mediafile: string) {
     return fetch(
-      `http://${serverconfig.hostname}/api/queue?mediafile=${encodeURIComponent(mediafile)}`,
+      `http://${serverconfig.hostname}/api/queue?mediafile=${
+        encodeURIComponent(mediafile)
+      }`,
       { method: "POST" },
     );
   }
@@ -15,6 +30,10 @@ function App() {
   return (
     <div className="App">
       <span className="title">GNUPlex</span>
+      <div className="controls">
+        <input type="button" value="Play" onClick={play} />
+        <input type="button" value="Pause" onClick={pause} />
+      </div>
       {mediafiles.map((mediafile: string, i: number) => (
         <a
           className="mediafile"
