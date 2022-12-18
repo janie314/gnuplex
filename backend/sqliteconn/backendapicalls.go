9a98e8651c6ff2eb4a655c765c8f317ff22cd877
j <j>
2022-12-18 10:39:21 -0600

    Lock everything

--- backend/sqliteconn/backendapicalls.go
+++ backend/sqliteconn/backendapicalls.go
@@ -6,17 +6,20 @@ import (
 	"io/fs"
 	"os"
 	"path/filepath"
+	"sync"
 )
 
-func ScanLib(db *sql.DB) error {
+func ScanLib(db *sql.DB, mu *sync.Mutex) error {
+	mu.Lock()
+	defer mu.Unlock()
 	var reterr error
-	mediadirs := GetMediadirs(db)
+	mediadirs := GetMediadirs(db, mu)
 	for _, mediadir := range mediadirs {
 		dir, err := os.Stat(mediadir)
 		if (err == nil) && dir.IsDir() {
 			err = filepath.WalkDir(mediadir, func(path string, entry fs.DirEntry, err error) error {
 				if err == nil && (!entry.IsDir()) {
-					return AddMedia(db, path)
+					return AddMedia(db, mu, path)
 				} else {
 					fmt.Fprintln(os.Stderr, "Walkdir prob: ", mediadir)
 					return err
@@ -38,7 +41,9 @@ func ScanLib(db *sql.DB) error {
 	return reterr
 }
 
-func AddHist(db *sql.DB, mediafile string) error {
+func AddHist(db *sql.DB, mu *sync.Mutex, mediafile string) error {
+	mu.Lock()
+	defer mu.Unlock()
 	_, err := db.Exec("insert into history (mediafile) values (?);", mediafile)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "AddHist err", err)
@@ -46,7 +51,9 @@ func AddHist(db *sql.DB, mediafile string) error {
 	return err
 }
 
-func AddMedia(db *sql.DB, mediafile string) error {
+func AddMedia(db *sql.DB, mu *sync.Mutex, mediafile string) error {
+	mu.Lock()
+	defer mu.Unlock()
 	_, err := db.Exec("insert or replace into medialist (filepath) values (?);", mediafile)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "AddMedia err", err)
@@ -54,7 +61,9 @@ func AddMedia(db *sql.DB, mediafile string) error {
 	return err
 }
 
-func GetMediadirs(db *sql.DB) []string {
+func GetMediadirs(db *sql.DB, mu *sync.Mutex) []string {
+	mu.Lock()
+	defer mu.Unlock()
 	rows, err := db.Query("select filepath from mediadirs order by filepath;")
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
@@ -76,7 +85,9 @@ func GetMediadirs(db *sql.DB) []string {
 	return res[:i]
 }
 
-func SetMediadirs(db *sql.DB, mediadirs []string) error {
+func SetMediadirs(db *sql.DB, mu *sync.Mutex, mediadirs []string) error {
+	mu.Lock()
+	defer mu.Unlock()
 	var err error
 	db.Exec("delete from mediadirs;")
 	for _, mediafile := range mediadirs {
@@ -88,7 +99,9 @@ func SetMediadirs(db *sql.DB, mediadirs []string) error {
 	return err
 }
 
-func GetMedialib(db *sql.DB) []string {
+func GetMedialib(db *sql.DB, mu *sync.Mutex) []string {
+	mu.Lock()
+	defer mu.Unlock()
 	rows, err := db.Query("select filepath from medialist order by filepath;")
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
@@ -110,7 +123,9 @@ func GetMedialib(db *sql.DB) []string {
 	return res[:i]
 }
 
-func Last25(db *sql.DB) []string {
+func Last25(db *sql.DB, mu *sync.Mutex) []string {
+	mu.Lock()
+	defer mu.Unlock()
 	rows, err := db.Query("select distinct mediafile from history order by id desc limit 25;")
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
