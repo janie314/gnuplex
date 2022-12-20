386720ad67ea9f7d9e72711b5308f2b1b04e102c
j <j>
2022-12-19 21:30:12 -0600

    LiteDB struct

--- backend/liteDB/liteDB.go
+++ backend/liteDB/liteDB.go
@@ -1,4 +1,4 @@
-package sqliteconn
+package liteDB
 
 import (
 	"database/sql"
@@ -17,42 +17,45 @@ type LiteDB struct {
 	Mu         *sync.Mutex
 }
 
-func Init(mu *sync.Mutex) *sql.DB {
+func Init() *LiteDB {
+	var db LiteDB
+	db.Mu = &sync.Mutex{}
+	db.Mu.Lock()
 	fmt.Println("Got Init LiteDB lock")
+	defer db.Mu.Unlock()
 	defer fmt.Println("Rem Init LiteDB lock")
-	mu.Lock()
-	defer mu.Unlock()
-	db, err := sql.Open("sqlite", "../tmp/gnuplex.sqlite3")
+	conn, err := sql.Open("sqlite", "../tmp/gnuplex.sqlite3")
+	db.SqliteConn = conn
 	if err != nil {
 		log.Fatal("Init LiteDB fatal error:", err)
 	}
-	_, err = db.Exec("create table if not exists pos_cache (filepath string not null primary key, pos int);")
+	_, err = db.SqliteConn.Exec("create table if not exists pos_cache (filepath string not null primary key, pos int);")
 	res := true
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "Init LiteDB error 1:", err)
 		res = false
 	}
-	_, err = db.Exec("create table if not exists history (id integer not null unique, mediafile	text, primary key(id AUTOINCREMENT));")
+	_, err = db.SqliteConn.Exec("create table if not exists history (id integer not null unique, mediafile	text, primary key(id AUTOINCREMENT));")
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "Init LiteDB error 2:", err)
 		res = false
 	}
-	_, err = db.Exec("create table if not exists medialist (filepath text not null,  primary key(filepath)) ;")
+	_, err = db.SqliteConn.Exec("create table if not exists medialist (filepath text not null,  primary key(filepath)) ;")
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "Init LiteDB error 3:", err)
 		res = false
 	}
-	_, err = db.Exec("create table if not exists mediadirs (filepath text not null, primary key(filepath)) ;")
+	_, err = db.SqliteConn.Exec("create table if not exists mediadirs (filepath text not null, primary key(filepath)) ;")
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "Init LiteDB error 4:", err)
 		res = false
 	}
-	_, err = db.Exec("create table if not exists version_info (key string not null primary key, value string);")
+	_, err = db.SqliteConn.Exec("create table if not exists version_info (key string not null primary key, value string);")
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "Init LiteDB error 5:", err)
 		res = false
 	}
-	_, err = db.Exec("insert or ignore into version_info values ('db_schema_version', ?);", consts.DBVersion)
+	_, err = db.SqliteConn.Exec("insert or ignore into version_info values ('db_schema_version', ?);", consts.DBVersion)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "Init LiteDB error 6:", err)
 		res = false
@@ -60,12 +63,12 @@ func Init(mu *sync.Mutex) *sql.DB {
 	if !res {
 		log.Fatal("I could not initialize the database...")
 	}
-	UpgradeDB(db)
-	return db
+	UpgradeDB(&db)
+	return &db
 }
 
-func UpgradeDB(db *sql.DB) error {
-	rows, err := db.Query("select value from version_info where key = 'db_schema_version';")
+func UpgradeDB(liteDB *LiteDB) error {
+	rows, err := liteDB.SqliteConn.Query("select value from version_info where key = 'db_schema_version';")
 	if err != nil {
 		log.Fatal("Upgrade db error:", err)
 	}
