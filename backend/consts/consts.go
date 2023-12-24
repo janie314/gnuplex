88e5396beded168e68bcd99275675021b6db2add
12ball <pub@janie.page>
2023-12-24 09:32:31 -0600

    Version bump

--- backend/consts/consts.go
+++ backend/consts/consts.go
@@ -1,7 +1,7 @@
 package consts
 
 const (
-	GNUPlexVersion      = "0.92 Currituck"
+	GNUPlexVersion      = "0.100 Sauron"
 	DBVersion           = 1
 	DevDBFilepath       = "../tmp/gnuplex.sqlite3"
 	ProdDBFilepath      = "/var/gnuplex/gnuplex.sqlite3"
