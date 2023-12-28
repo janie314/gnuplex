6892fef2b1d89455a7a7c7b15bfff7368c136832
12ball <pub@janie.page>
2023-12-27 18:29:34 -0600

    1703723374

--- frontend/src/components/MediaControls.tsx
+++ frontend/src/components/MediaControls.tsx
@@ -2,13 +2,14 @@ import { useEffect, useState } from "react";
 import { APICall } from "../lib/APICall.ts";
 import "../App.css";
 import "./CRUDPopup.css";
+import "./MediaControls.css";
 
 function MediaControls() {
   return (
     <div className="mediacontrols">
-      <input type="button" value="⥀" />
-      <input type="button" value="⏵" />
-      <input type="button" value="⥁" />
+      <input type="button" className="mediacontrol" value="⥀" />
+      <input type="button" className="mediacontrol" value="⏵" />
+      <input type="button" className="mediacontrol" value="⥁" />
     </div>
   );
 }
