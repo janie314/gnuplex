c63e658a248352c4c3170249c0fd98f6e67d000b
12ball <pub@janie.page>
2024-01-10 22:46:33 -0600

    1704948393

--- backend/server/apiEndpoints.go
+++ backend/server/apiEndpoints.go
@@ -11,49 +11,104 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+/*
+ * Types for various request and response JSON bodies
+ */
+type mediaBody struct {
+	Media string `json:"media"`
+}
+
+type volBody struct {
+	Vol float64 `json:"vol"`
+}
+
+type posBody struct {
+	Pos float64 `json:"pos"`
+	Inc bool    `json:"inc"`
+}
+
+type posResponse struct {
+	Pos    float64 `json:"pos"`
+	MaxPos float64 `json:"max_pos"`
+}
+
 func (srv *Server) initEndpoints(api_url_base string) {
+	/*
+	 * GET /version
+	 * Response: string
+	 *   GNUPlex version string.
+	 */
 	srv.Router.GET(api_url_base+"/version", func(c *gin.Context) {
 		c.JSON(http.StatusOK, consts.GNUPlexVersion)
 	})
+	/*
+	 * GET /paused
+	 * Response: boolean
+	 *   If current media is paused.
+	 */
 	srv.Router.GET(api_url_base+"/paused", func(c *gin.Context) {
 		paused, err := srv.mpv.IsPaused()
-		readQuery2HTTP(c, paused, err)
+		readQueryResponse(c, paused, err)
 	})
+	/*
+	 * POST /paused
+	 * Toggles video's play/pause status.
+	 * Response: boolean
+	 *   If current media is paused.
+	 */
 	srv.Router.POST(api_url_base+"/toggle", func(c *gin.Context) {
 		paused, err := srv.mpv.Toggle()
-		readQuery2HTTP(c, paused, err)
+		readQueryResponse(c, paused, err)
 	})
+	/*
+	 * GET /media
+	 * Response: string
+	 *   Current media file.
+	 */
 	srv.Router.GET(api_url_base+"/media", func(c *gin.Context) {
 		media, err := srv.mpv.GetMedia()
-		readQuery2HTTP(c, media, err)
+		readQueryResponse(c, media, err)
 	})
+	/*
+	 * POST /media
+	 * Body:
+	 *   {
+	 *     media: string;
+	 *   }
+	 */
 	srv.Router.POST(api_url_base+"/media", func(c *gin.Context) {
-		mediafile := c.Query("mediafile")
-		if mediafile == "" {
+		var media mediaBody
+		c.BindJSON(&media)
+		if media.Media == "" {
 			c.String(http.StatusBadRequest, "empty mediafile string")
 		} else {
-			err := srv.mpv.SetMedia(mediafile)
+			err := srv.mpv.SetMedia(media.Media)
 			if err == nil {
-				srv.DB.AddHist(mediafile, false)
+				srv.DB.AddHist(media.Media, false)
 			}
-			writeQuery2HTTP(c, err)
+			writeQueryResponse(c, err)
 		}
 	})
+	/*
+	 * GET /media
+	 * Response: number
+	 *   Current volume.
+	 */
 	srv.Router.GET(api_url_base+"/vol", func(c *gin.Context) {
 		vol, err := srv.mpv.GetVolume()
-		readQuery2HTTP(c, vol, err)
+		readQueryResponse(c, vol, err)
 	})
+	/*
+	 * POST /vol
+	 * Body:
+	 *   {
+	 *     vol: number;
+	 *   }
+	 */
 	srv.Router.POST(api_url_base+"/vol", func(c *gin.Context) {
-		param := c.Query("vol")
-		if param == "" {
-			c.String(http.StatusBadRequest, "empty vol string")
-		} else {
-			vol, err := strconv.Atoi(param)
-			if err != nil {
-				c.String(http.StatusBadRequest, "bad vol string")
-			}
-			writeQuery2HTTP(c, srv.mpv.SetVolume(float64(vol)))
-		}
+		var vol volBody
+		c.BindJSON(&vol)
+		writeQueryResponse(c, srv.mpv.SetVolume(vol.Vol))
 	})
 	// TODO fold this into /pos
 	srv.Router.POST(api_url_base+"/incpos", func(c *gin.Context) {
@@ -65,7 +120,7 @@ func (srv *Server) initEndpoints(api_url_base string) {
 			if err != nil {
 				c.String(http.StatusBadRequest, "bad inc string")
 			}
-			writeQuery2HTTP(c, srv.mpv.IncPos(float64(inc)))
+			writeQueryResponse(c, srv.mpv.IncPos(float64(inc)))
 		}
 	})
 	// TODO fold into new framework
@@ -105,25 +160,40 @@ func (srv *Server) initEndpoints(api_url_base string) {
 			}
 		}
 	})
+	/*
+	 * GET /pos
+	 * Response: number
+	 *   Current position (seconds).
+	 */
 	srv.Router.GET(api_url_base+"/pos", func(c *gin.Context) {
 		pos, err := srv.mpv.GetPos()
-		readQuery2HTTP(c, pos, err)
+		readQueryResponse(c, pos, err)
 	})
+	/*
+	 * POST /pos
+	 * Body:
+	 *   {
+	 *     pos: number;
+	 *     inc: boolean;   // whether this is an absolute position or an increment from the current position
+	 *   }
+	 * Response: if no error, responds with the following body with the current position:
+	 *   {
+	 *     pos: number;
+	 *   }
+	 */
 	srv.Router.POST(api_url_base+"/pos", func(c *gin.Context) {
-		param := c.Query("pos")
-		if param == "" {
-			c.String(http.StatusBadRequest, "empty pos string")
+		var pos posBody
+		var err error
+		if pos.Inc {
+			err = srv.mpv.IncPos(pos.Pos)
 		} else {
-			pos, err := strconv.Atoi(param)
-			if err != nil {
-				c.String(http.StatusBadRequest, "bad pos string")
-			}
-			err = srv.mpv.SetPos(float64(pos))
-			if err != nil {
-				c.Data(http.StatusOK, "application/json", nil)
-			} else {
-				c.Data(http.StatusOK, "application/json", nil)
-			}
+			err = srv.mpv.SetPos(pos.Pos)
+		}
+		if err != nil {
+			newPos, err := srv.mpv.GetPos()
+			readQueryResponse(c, newPos, err)
+		} else {
+			c.Data(http.StatusOK, "application/json", nil)
 		}
 	})
 	srv.Router.GET(api_url_base+"/last25", func(c *gin.Context) {
@@ -138,7 +208,8 @@ func (srv *Server) initEndpoints(api_url_base string) {
 	})
 }
 
-func readQuery2HTTP[T mpv.ResponseDatum](c *gin.Context, val T, err error) {
+// cast an mpvcmd read query into a Gin response
+func readQueryResponse[T mpv.ResponseDatum](c *gin.Context, val T, err error) {
 	if err != nil {
 		log.Println("Error", err)
 		c.JSON(http.StatusInternalServerError, nil)
@@ -147,7 +218,8 @@ func readQuery2HTTP[T mpv.ResponseDatum](c *gin.Context, val T, err error) {
 	}
 }
 
-func writeQuery2HTTP(c *gin.Context, err error) {
+// cast an mpvcmd write query into a Gin response
+func writeQueryResponse(c *gin.Context, err error) {
 	if err != nil {
 		log.Println("Error", err)
 		c.JSON(http.StatusInternalServerError, nil)
