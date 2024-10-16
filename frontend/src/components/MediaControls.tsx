ae36656e9af7560a738844c601670b26f13e16a2
janie314 <j@janie.page>
2024-10-15 19:42:06 -0500

    1729039326

--- frontend/src/components/MediaControls.tsx
+++ frontend/src/components/MediaControls.tsx
@@ -1,4 +1,6 @@
 import { APICall } from "../lib/APICall";
+import play from "../assets/play.svg";
+import pause from "../assets/pause.svg";
 
 function MediaControls(props: {
   mediadirInputPopup: boolean;
@@ -14,27 +16,65 @@ function MediaControls(props: {
 }) {
   return (
     <div className="flex flex-col justify-center p-1">
-      <div className="flex flex-row justify-center mt-3">
-        <input
-          className="m-1 p-1 border border-solid border-black hover:bg-cyan-300"
-          type="button"
-          value="⏵"
-          onClick={() =>
-            APICall.play()
-              .then(() => APICall.sleep(2000))
-              .then(() => props.setVolPosToggle(!props.volPosToggle))
-          }
-        />
-        <input
-          className="m-1 p-1 border border-solid border-black hover:bg-cyan-300"
-          type="button"
-          value="⏸"
-          onClick={() =>
-            APICall.pause()
-              .then(() => APICall.sleep(2000))
-              .then(() => props.setVolPosToggle(!props.volPosToggle))
-          }
-        />
+      <div className="flex flex-row items-center justify-center mt-3">
+        <div className="mr-1">
+          <button
+            type="button"
+            className="p-2 w-8 border border-solid border-black hover:bg-cyan-300"
+            onClick={() =>
+              APICall.play()
+                .then(() => APICall.sleep(2000))
+                .then(() => props.setVolPosToggle(!props.volPosToggle))
+            }
+          >
+            <img src={play} alt="Play icon" />
+          </button>
+        </div>
+        <div className="mr-2">
+          <button
+            type="button"
+            className="p-2 w-8 border border-solid border-black hover:bg-cyan-300"
+            onClick={() =>
+              APICall.pause()
+                .then(() => APICall.sleep(2000))
+                .then(() => props.setVolPosToggle(!props.volPosToggle))
+            }
+          >
+            <img src={pause} alt="Pause icon" />
+          </button>
+        </div>
+
+        <div className="flex flex-col p-1">
+          <div className="flex flex-row">
+            <span className="mx-1">Pos</span>
+            <input
+              type="range"
+              min={0}
+              max={props.startPos + props.timeRemaining}
+              value={props.pos}
+              className="range range-xs"
+              // @ts-ignore
+              onChange={(e) => props.setPos(e.target.value)}
+              onMouseUp={() => APICall.setOriginPos(props.pos)}
+            />
+            <span className="mx-1">{timeFormat(props.pos)}</span>
+          </div>
+
+          <div className="flex flex-row mt-3">
+            <span className="mx-1">Vol</span>
+            <input
+              type="range"
+              min={0}
+              max={120}
+              value={props.vol}
+              className="range range-xs"
+              // @ts-ignore
+              onChange={(e) => props.setVol(e.target.value)}
+              onMouseUp={() => APICall.setOriginVol(props.vol)}
+            />
+            <span className="mx-1">{props.vol}</span>
+          </div>
+        </div>
       </div>
       <div className="flex flex-row justify-center mt-3">
         <input
@@ -54,35 +94,6 @@ function MediaControls(props: {
             APICall.setOriginMedia(url);
           }}
         />
-      </div>
-      <div className="flex flex-row justify-center items-center mt-3">
-        <span className="mx-1">Pos</span>
-        <input
-          type="range"
-          min={0}
-          max={props.startPos + props.timeRemaining}
-          value={props.pos}
-          className="range range-xs"
-          // @ts-ignore
-          onChange={(e) => props.setPos(e.target.value)}
-          onMouseUp={() => APICall.setOriginPos(props.pos)}
-        />
-        <span className="mx-1">{timeFormat(props.pos)}</span>
-      </div>
-
-      <div className="flex flex-row justify-center items-center mt-3">
-        <span className="mx-1">Vol</span>
-        <input
-          type="range"
-          min={0}
-          max={120}
-          value={props.vol}
-          className="range range-xs"
-          // @ts-ignore
-          onChange={(e) => props.setVol(e.target.value)}
-          onMouseUp={() => APICall.setOriginVol(props.vol)}
-        />
-        <span className="mx-1">{props.vol}</span>
       </div>
     </div>
   );
