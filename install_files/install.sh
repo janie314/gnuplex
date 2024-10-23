8938fe045e58d79788adecaf28e05b99e484393e
janie314 <j@janie.page>
2024-10-23 18:54:03 -0500

    Finalize install script

--- install_files/install.sh
+++ install_files/install.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
-printf "Install dir (default $HOME/.local/bin): "
-read install_dir
+set -e
+printf "Install dir (default %s/.local/bin): " "$HOME"
+read -r install_dir
 if [ "$install_dir" = "" ]
 then
   install_dir="$HOME/.local/bin"
 fi
 mkdir -p "$install_dir"
 git clone -b release https://github.com/janie314/gnuplex.git "$install_dir/gnuplex-code"
+cd "$install_dir"
+ln -s gnuplex-code/backend/bin/gnuplex .
