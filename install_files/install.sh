3c3912f338841d90f9c081b43ee6a61010ecb162
janie314 <j@janie.page>
2024-10-27 19:28:08 -0500

    install.sh hotfix 2

--- install_files/install.sh
+++ install_files/install.sh
@@ -15,6 +15,6 @@ fi
 mkdir -p "$install_dir"
 git clone -b release https://github.com/janie314/gnuplex.git "$install_dir/gnuplex-code"
 mkdir -p "$install_dir/gnuplex-code/backend/bin"
-curl -o "$install_dir/gnuplex-code/backend/bin/gnuplex" https://gnuplex.janie.page/gnuplex
+curl --output-dir "$install_dir/gnuplex-code/backend/bin" -O https://gnuplex.janie.page/gnuplex
 cd "$install_dir"
 ln -s gnuplex-code/backend/bin/gnuplex .
