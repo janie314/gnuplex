f565cd8234ede4b6f9fe201b4b4edac039d184d7
janie314 <j@janie.page>
2024-10-27 19:18:59 -0500

    Switch install to gnuplex.janie.page

--- install_files/install.sh
+++ install_files/install.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 set -e
+# dependencies
+echo "checking if git exists"
+command -v git
+echo "checking if curl exists"
+command -v curl
+# continue
 printf "Install dir (default %s/.local/bin): " "$HOME"
 read -r install_dir
 if [ "$install_dir" = "" ]
@@ -8,5 +14,6 @@ then
 fi
 mkdir -p "$install_dir"
 git clone -b release https://github.com/janie314/gnuplex.git "$install_dir/gnuplex-code"
+curl -o "$install_dir/gnuplex-code/backend/bin/gnuplex" https://gnuplex.janie.page/gnuplex
 cd "$install_dir"
 ln -s gnuplex-code/backend/bin/gnuplex .
