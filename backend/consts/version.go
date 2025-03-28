67482d43042d002f9b1b5e9a14d36deedf521e92
12ball <pub@janie.page>
2025-03-28 13:44:58 -0500

    Change -version output to be JSON

--- backend/consts/version.go
+++ backend/consts/version.go
@@ -3,3 +3,8 @@ package consts
 const (
 	Version = "1743177626"
 )
+
+type VersionInfo struct {
+	Version    string `json:"version"`
+	SourceHash string `json:"source_hash"`
+}
--- backend/gnuplex/webEndpoints.go
+++ backend/gnuplex/webEndpoints.go
@@ -41,11 +41,6 @@ type SubBody struct {
 	ID      int64 `json:"id,omitempty"`
 }
 
-type VersionRes struct {
-	Version    string `json:"version"`
-	SourceHash string `json:"source_hash"`
-}
-
 // Initialize the web server's HTTP Endpoints
 func (gnuplex *GNUPlex) InitWebEndpoints(prod bool, staticFiles, sourceHash string) {
 	gnuplex.Router.GET("/", func(c *gin.Context) {
@@ -56,7 +51,7 @@ func (gnuplex *GNUPlex) InitWebEndpoints(prod bool, staticFiles, sourceHash stri
 	})
 	gnuplex.Router.Static("/home", staticFiles)
 	gnuplex.Router.GET("/api/version", func(c *gin.Context) {
-		c.JSON(http.StatusOK, VersionRes{Version: consts.Version, SourceHash: sourceHash})
+		c.JSON(http.StatusOK, consts.VersionInfo{Version: consts.Version, SourceHash: sourceHash})
 	})
 	gnuplex.Router.POST("/api/play", func(c *gin.Context) {
 		if err := gnuplex.MPV.Play(); err != nil {
