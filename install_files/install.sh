33ba5463f4206b73bed8dac5fe957bd9ed119a08
12ball <pub@janie.page>
2025-08-10 12:29:09 -0700

    Install script work to support alpine

--- install_files/install.sh
+++ install_files/install.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 set -e
-echo "Installing GNUPlex..."
-printf "\n"
+PATH="PATH:$HOME/.local/bin"
+echo "installing GNUPlex..."
 # dependencies
-echo "checking if git exists"
-command -v git
-echo "checking if curl exists"
-command -v curl
+echo "checking if GNUPlex exists; quitting if so. run \`gnuplex -upgrade\` to update GNUPlex instead"
+if command -v gnuplex 1>/dev/null
+then
+  exit 1
+fi
+echo "checking if git exists; quitting if not"
+command -v git 
+echo "checking if curl exists; quitting if not"
+command -v curl 1>/dev/null
 # continue
 echo "All needed commands exist, continuing..."
 printf "Install dir (default %s/.local/bin): " "$HOME"
@@ -16,16 +21,38 @@ then
   install_dir="$HOME/.local/bin"
 fi
 mkdir -p "$install_dir"
-git clone -b release https://github.com/janie314/gnuplex.git "$install_dir/gnuplex-code"
+# find if glibc or musl
+libc=$(ldd /bin/ls | grep 'musl' | head -1 | cut -d ' ' -f1)
+if [ -z "$libc" ]
+then
+  libc="glibc"
+else
+  libc="musl"
+fi
+# find if x86_64
+if [ "$(uname --machine)" != "x86_64" ]
+then
+  echo "Unsupported architecture: $(uname --machine)"
+  exit 1
+fi
+# clone from git
+git clone -b "release-linux-$libc-x86_64" https://github.com/janie314/gnuplex.git "$install_dir/gnuplex-code"
 cd "$install_dir"
 ln -s gnuplex-code/backend/bin/gnuplex .
-echo "Installing systemd user service..."
-mkdir -p "$HOME/.config/systemd/user"
-install_dir_replace=$(echo "$install_dir" | sed -e 's~/~\\/~g')
-cat "$install_dir/gnuplex-code/install_files/gnuplex.service" | sed -e "s/__DIR__/$install_dir_replace/" >"$HOME/.config/systemd/user/gnuplex.service"
-printf "\n"
-echo "Done."
+# systemd service
+if command -v journalctl 1>/dev/null
+then
+  echo "Installing systemd user service..."
+  mkdir -p "$HOME/.config/systemd/user"
+  install_dir_replace=$(echo "$install_dir" | sed -e 's~/~\\/~g')
+  cat "$install_dir/gnuplex-code/install_files/gnuplex.service" | sed -e "s/__DIR__/$install_dir_replace/" >"$HOME/.config/systemd/user/gnuplex.service"
+  printf "\n"
+  echo "Done."
+  printf "\n\n" 
+  echo "To run GNUPlex persistently, run \`systemctl --user enable --now gnuplex\` and navigate to http://localhost:40000/"
+  printf "\n\n"
+  echo "Start GNUPlex ad-hoc with \`$install_dir/gnuplex"\`
+else
+  echo "To start GNUPlex, run \`"$install_dir/gnuplex"\` and navigate to http://localhost:40000/."
+fi
 printf "\n\n" 
-echo "To use GNUPlex locally, run \`"$install_dir/gnuplex"\` and navigate to http://localhost:40000/."
-printf "\n\n" 
-echo "To run GNUPlex persistently, run \`systemctl --user enable --now gnuplex\`"
