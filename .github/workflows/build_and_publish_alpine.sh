ddf642201edd3c862d8c2ebb322c49eab42751fa
12ball <pub@janie.page>
2025-08-23 12:01:34 -0500

    Release fixes

--- .github/workflows/build_and_publish_alpine.sh
+++ .github/workflows/build_and_publish_alpine.sh
@@ -4,7 +4,7 @@ echo "$1" >>/tmp/gnuplex-deploy
 apk add uv git go curl bash
 git clone --depth 1 --branch release https://github.com/janie314/gnuplex
 cd gnuplex
-uv run make.py go_build
+uv run make.py build
 git add .
 git commit -m "$version_output"
-GIT_SSH_COMMAND="ssh -i /tmp/gnuplex-deploy" git push origin release-linux-x86_64
+GIT_SSH_COMMAND="ssh -i /tmp/gnuplex-deploy" git push origin release-linux-musl-x86_64
