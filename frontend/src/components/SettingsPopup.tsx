2c037b0d3e418984dd8f3f41895d3e3376911aed
Jane Davis <61095764+janie314@users.noreply.github.com>
2025-12-24 19:30:10 -0600

    Implement filter controls (#225)
    Implement filters

---
+++ frontend/src/components/SettingsPopup.tsx
@@ -0,0 +1,72 @@
+import "./Popup.css";
+import { API } from "../lib/API";
+
+function SettingsPopup(props: { visible: boolean; closeHook: () => void }) {
+  if (props.visible) {
+    return (
+      <div className="popup bg-white dark:bg-stone-800 m-5">
+        <div className="flex flex-col">
+          <input
+            type="button"
+            value="Black and White Filter"
+            className="btn-standard m-1 min-w-[11ch]"
+            onClick={() => {
+              API.setFilter("bw");
+            }}
+          />{" "}
+          <input
+            type="button"
+            value="Grainy Filter"
+            className="btn-standard m-1 min-w-[11ch]"
+            onClick={() => {
+              API.setFilter("grainy");
+            }}
+          />
+          <input
+            type="button"
+            value="8-Bit Filter"
+            className="btn-standard m-1 min-w-[11ch]"
+            onClick={() => {
+              API.setFilter("8bit");
+            }}
+          />{" "}
+          <input
+            type="button"
+            value="Mirror Filter"
+            className="btn-standard m-1 min-w-[11ch]"
+            onClick={() => {
+              API.setFilter("mirror");
+            }}
+          />
+          <input
+            type="button"
+            value="Sepia Filter"
+            className="btn-standard m-1 min-w-[11ch]"
+            onClick={() => {
+              API.setFilter("sepia");
+            }}
+          />
+          <input
+            type="button"
+            value="Reset Filter"
+            className="btn-standard m-1 min-w-[11ch]"
+            onClick={() => {
+              API.setFilter("neutral");
+            }}
+          />
+          <input
+            type="button"
+            value="Cancel"
+            className="btn-standard m-1 min-w-[11ch]"
+            onClick={() => {
+              props.closeHook();
+            }}
+          />
+        </div>
+      </div>
+    );
+  }
+  return null;
+}
+
+export { SettingsPopup };
